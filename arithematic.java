47a281d191a10430866558aed47762e9b046a0cb
amanver02 <155369942+amanver02@users.noreply.github.com>
2024-05-30 11:50:56 +0530

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,12 @@
+public class arithematic {
+    public static void main(String[]args){
+         int x=5;
+         int y=3;
+         System.out.println(x*y);
+         System.out.println(x+y);
+         System.out.println(x-y);
+        System.out.println(x%y);
+        System.out.print(y-x);
+        System.out.print(x/y);
+    }
+}
